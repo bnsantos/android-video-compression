9aaedc62079894277aaa93b2c6e23a625ee139eb
Bruno Santos <brunonascimentosantos@gmail.com>
2016-05-17 11:29:39 -0300

    storage permission

--- app/src/main/java/com/bnsantos/video/recording/MainActivity.java
+++ app/src/main/java/com/bnsantos/video/recording/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bnsantos.video.recording;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.media.MediaMetadataRetriever;
@@ -8,22 +10,32 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bnsantos.video.recording.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
   private static final String BUNDLE_LENGTH = "BUNDLE_VIDEO_LENGTH";
   private static final String BUNDLE_SIZE = "BUNDLE_VIDEO_SIZE";
   private static final String BUNDLE_QUALITY = "BUNDLE_VIDEO_QUALITY";
+  private static final String BUNDLE_COMPRESS = "BUNDLE_VIDEO_COMPRESS";
+
+  private static final String PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+  private static final int REQUEST_STORAGE_PERMISSION = 123;
   private static final int ACTION_RECORD_VIDEO = 3;
+
   private int mMaxLength = 60;
   private int mQuality = 0;
   private long mMaxSize = 5; //In Mb
   private static final long K = 1024;
+  private boolean mCompress = false;
 
   private Uri mVideoUri;
 
@@ -37,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     setSupportActionBar(binding.toolbar);
     binding.fab.setOnClickListener(this);
 
+    binding.compress.setOnCheckedChangeListener(this);
     updateUI();
   }
 
@@ -64,6 +77,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     binding.maxLength.setText(Integer.toString(mMaxLength));
     binding.maxSize.setText(Long.toString(mMaxSize));
     binding.quality.setChecked(mQuality==1);
+    binding.compress.setChecked(mCompress);
 
     if(mVideoUri!=null){
       binding.resultLayout.setVisibility(View.VISIBLE);
@@ -103,6 +117,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     mMaxLength = Integer.parseInt(binding.maxLength.getText().toString());
     mMaxSize = Long.parseLong(binding.maxSize.getText().toString());
     mQuality = binding.quality.isChecked()?1:0;
+    mCompress = binding.compress.isChecked();
   }
 
   @Override
@@ -131,7 +146,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       if (data!=null) {
         mVideoUri = data.getData();
         updateUI();
+        if(mCompress){
+          compress();
+        }
       }
+    }
+  }
+
+  @Override
+  public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+    if(!hasPermission()){
+      requestPermission();
+    }
+    mCompress = b;
+  }
+
+  private void compress(){
+    if (hasPermission()) {
+      Toast.makeText(MainActivity.this, R.string.compressing, Toast.LENGTH_SHORT).show();
+    }else{
+      requestPermission();
+    }
+  }
+
+  private boolean hasPermission(){
+    return ActivityCompat.checkSelfPermission(this, PERMISSION) == PackageManager.PERMISSION_GRANTED;
+  }
+
+  private void requestPermission(){
+    ActivityCompat.requestPermissions(this, new String[]{PERMISSION}, REQUEST_STORAGE_PERMISSION);
+  }
+  
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    if(requestCode== REQUEST_STORAGE_PERMISSION){
+      // Check if the only required permission has been granted
+      if (!(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), R.string.permission_storage_need,
+            Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.allow, new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{PERMISSION}, REQUEST_STORAGE_PERMISSION);
+              }
+            });
+        ((TextView)snackbar.getView().findViewById(android.support.design.R.id.snackbar_text)).setMaxLines(5);
+        snackbar.show();
+      }
+    }else{
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
   }
 }
